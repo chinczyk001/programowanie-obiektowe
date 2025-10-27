aab10106d825b00874aab9343601a8f4ccf0c7b7
chinczyk001 <you@example.com>
2025-10-27 16:06:58 +0100

    klasaOsoba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Osoba osoba1 = new Osoba();
+        System.out.println(osoba1.imie);
+        osoba1.imie = "Jaś";
+        System.out.println(osoba1.imie);
+        osoba1.setWiek(123);
+        System.out.println(osoba1.getWiek());
+        osoba1.setWiek(23);
+        System.out.println(osoba1.getWiek());
+        System.out.println(osoba1);
+        Osoba osoba2 = new Osoba("Asia",22);
+        System.out.println(osoba2);
+
     }
 }
---
+++ src/Osoba.java
@@ -0,0 +1,41 @@
+public class Osoba {
+    public String imie;
+    private int wiek; //hermetyzacja (enkapsulacja) - ograniczony dostep do pol metod
+    //private tylko w tej klasie
+    //public dostepne wszedzie
+    //protected dostepne w tej klasie i klasie potomnej (w Javie takze w ramach pakietu)
+    //brak modyfiikatora dostepu - dostepne w pakiecie
+
+    //przeciazanie konstruktorow
+    public Osoba() {
+        imie="";
+        wiek =0;
+    }
+
+    public Osoba(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+    //do wieku metody dostepowe
+
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        if(wiek>0 && wiek < 100) {
+            this.wiek = wiek;
+        }else {
+        this.wiek = 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", wiek=" + wiek +
+                '}';
+    }
+}
